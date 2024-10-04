afc4081ad9790c5c93d382d84178a8f095cf372c
Yudi Matsuguma <yudimatsuguma@gmail.com>
2024-10-04 01:53:25 -0300

    faet: adicionado titulo no modal do formulario de estacionamento

--- src/app/components/parking-lot/components/park-vehicle-dialog/park-vehicle-dialog.component.ts
+++ src/app/components/parking-lot/components/park-vehicle-dialog/park-vehicle-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { ParkVehicleFormComponent } from '../../../park-vehicle-form/park-vehicle-form.component';
 
 @Component({
@@ -8,7 +10,9 @@ import { ParkVehicleFormComponent } from '../../../park-vehicle-form/park-vehicl
   standalone: true,
   templateUrl: './park-vehicle-dialog.component.html',
   imports: [
+    MatButtonModule,
     MatCardModule,
+    MatIconModule,
     ParkVehicleFormComponent,
   ],
 })
