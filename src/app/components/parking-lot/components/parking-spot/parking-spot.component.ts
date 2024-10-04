fe5f73085953899d852cee5278a085e457ba7bd3
Yudi Matsuguma <yudimatsuguma@gmail.com>
2024-10-04 00:41:15 -0300

    feat: ajustado componente da vaga para exibir icone quando estiver ocupada

--- src/app/components/parking-lot/components/parking-spot/parking-spot.component.ts
+++ src/app/components/parking-lot/components/parking-spot/parking-spot.component.ts
@@ -1,11 +1,22 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-parking-spot',
   standalone: true,
   templateUrl: './parking-spot.component.html',
-  imports: [],
+  imports: [
+    MatIconModule,
+    UpperCasePipe,
+  ],
 })
 export class ParkingSpotComponent {
   @Input({ required: true }) spotName!: string;
+  @Input({ required: true }) isOccupied!: boolean;
+  @Input() plate!: string;
+
+  openDialog() {
+    console.log('Open dialog');
+  }
 }
