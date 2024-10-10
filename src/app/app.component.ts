5ee787bd5b73eaa7e47c910ff98227deaa0d0095
Yudi Matsuguma <yudimatsuguma@gmail.com>
2024-10-10 00:12:04 -0300

    build: instalado ngx-spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent, NgxSpinnerModule],
   template: `
     <app-toolbar />
     <router-outlet />
+    <ngx-spinner />
   `,
 })
 export class AppComponent { }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { provideSpinnerConfig } from 'ngx-spinner';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -12,5 +13,8 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideEnvironmentNgxMask(),
     provideHttpClient(),
+    provideSpinnerConfig({
+      type: 'ball-clip-rotate',
+    }),
   ],
 };
